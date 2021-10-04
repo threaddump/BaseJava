29421351ddf3f48e5bea7765e305329b5782427a
Eugene Logunov <eugene.logunov@gmail.com>
2021-10-05 00:00:50 +0300

    Lesson 3, part 1: Storage interface

--- src/com/basejava/webapp/MainTestArrayStorage.java
+++ src/com/basejava/webapp/MainTestArrayStorage.java
@@ -2,12 +2,13 @@ package com.basejava.webapp;
 
 import com.basejava.webapp.model.Resume;
 import com.basejava.webapp.storage.ArrayStorage;
+import com.basejava.webapp.storage.Storage;
 
 /**
  * Test for your com.basejava.webapp.storage.ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    private static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private static final Storage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
---
+++ src/com/basejava/webapp/storage/Storage.java
@@ -0,0 +1,23 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+public interface Storage {
+
+    void clear();
+
+    void update(Resume r);
+
+    void save(Resume r);
+
+    Resume get(String uuid);
+
+    void delete(String uuid);
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll();
+
+    int size();
+}

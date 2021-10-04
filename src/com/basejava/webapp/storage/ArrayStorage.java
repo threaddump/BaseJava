7bfd9ab09c45e79100de0ea953f12e6ab75c72a0
Eugene Logunov <eugene.logunov@gmail.com>
2021-10-04 22:45:48 +0300

    Lesson 2: H/W - created packages; ArrayStorage - optimized delete(), added update(), added some checks to get()/update()/save()/delete()

---
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -0,0 +1,83 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+    private int size;
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void save(Resume r) {
+        if (findIndex(r.getUuid()) != -1) {
+            System.err.println("ArrayStorage.save(): duplicate uuid " + r.getUuid() + ". Use update() instead");
+            return;
+        }
+        if (size == storage.length) {
+            System.err.println("ArrayStorage.save(): out of free space");
+            return;
+        }
+        storage[size++] = r;
+    }
+
+    public Resume get(String uuid) {
+        final int idx = findIndex(uuid);
+        if (idx != -1) {
+            return storage[idx];
+        } else {
+            System.err.println("ArrayStorage.get(): uuid " + uuid + " not found");
+            return null;
+        }
+    }
+
+    public void update(Resume r) {
+        final int idx = findIndex(r.getUuid());
+        if (idx != -1) {
+            storage[idx] = r;
+        } else {
+            System.err.println("ArrayStorage.update(): uuid " + r.getUuid() + " not found");
+        }
+    }
+
+    public void delete(String uuid) {
+        final int idx = findIndex(uuid);
+        if (idx != -1) {
+            storage[idx] = storage[size - 1];
+            storage[--size] = null;
+            return;
+        } else {
+            System.err.println("ArrayStorage.delete(): uuid " + uuid + " not found");
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    /**
+     * @return Index of matching element of storage[], or -1 (not found)
+     */
+    private int findIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}

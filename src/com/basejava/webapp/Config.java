10b5c5a551a040e64b05265b101f6fb2cbd9b328
Eugene Logunov <eugene.logunov@gmail.com>
2021-10-09 16:26:49 +0300

    added html+css template for further development; updated Config so that it could be loaded within servlet

--- src/com/basejava/webapp/Config.java
+++ src/com/basejava/webapp/Config.java
@@ -2,24 +2,22 @@ package com.basejava.webapp;
 
 import com.basejava.webapp.storage.factory.StorageConfig;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
     // keep in mind the order of initialization
-    private static final File CONFIG_FILE = new File("./config/resumes.properties");
+    private static final String CONFIG_FILE = "/resumes.properties";
     private static final Config INSTANCE = new Config();
 
     private final Properties props = new Properties();
 
     private Config() {
-        try (InputStream is = new FileInputStream(CONFIG_FILE)) {
+        try (InputStream is = Config.class.getResourceAsStream(CONFIG_FILE)) {
             props.load(is);
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + CONFIG_FILE.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + CONFIG_FILE);
         }
     }
 
--- src/com/basejava/webapp/web/ResumeServlet.java
+++ src/com/basejava/webapp/web/ResumeServlet.java
@@ -1,13 +1,37 @@
 package com.basejava.webapp.web;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import com.basejava.webapp.storage.Storage;
+import com.basejava.webapp.storage.factory.StorageFactory;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ResumeServlet extends HttpServlet {
+
+    private Storage storage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = StorageFactory.getStorage();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
 
+        response.getWriter().write(storage.getClass().getName());
+
+        /*
+        String name = request.getParameter("name");
+        response.getWriter().write("Hello Resumes");
+        */
     }
 
     @Override
